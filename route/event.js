4f06ab8275c0490a4d7fab640f5d2ff7b4b389d1
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-11-22 12:20:20 -0300

    Refactor balance to account

--- route/event.js
+++ route/event.js
@@ -2,10 +2,13 @@ const Router = require('express').Router
 const router = new Router()
 
 router.get('/', queryEvents)
+router.get('/account/:address', findEventsByAccount)
 router.get('/:id', findEventById)
 
 function queryEvents (req, res, next) {
-  req.logger.info('Querying events', req.query)
+  req.logger.info(`Querying events: ${JSON.stringify(req.query)}`)
+
+  console.log(req.query)
 
   req.model('Event').countAndFind(req.query)
     .skip(req.skip)
@@ -21,9 +24,9 @@ function queryEvents (req, res, next) {
 }
 
 function findEventById (req, res, next) {
-  req.logger.info(`Finding event with id ${req.params.id}`)
+  req.logger.info(`Finding event with id ${req.params.address}`)
 
-  req.model('Event').findById(req.params.id)
+  req.model('Event').findById(req.params.address)
     .then((event) => {
       if (!event) { return res.status(404).end() }
 
@@ -31,6 +34,29 @@ function findEventById (req, res, next) {
       return res.send(event)
     })
     .catch(err => next(err))
+}
+
+function findEventsByAccount (req, res, next) {
+  req.logger.info(`Finding events with account ${req.params.address}`)
+
+  const query = { $or: [
+    { 'metaData.returnValues._from': req.params.address },
+    { 'metaData.returnValues._to': req.params.address }
+  ]}
+
+  console.log(query)
+
+  req.model('Event').countAndFind(query)
+    .skip(req.skip)
+    .limit(req.limit)
+    .sort(req.sort)
+    .lean()
+    .exec((err, events, count) => {
+      if (err) { return next(err) }
+
+      req.logger.verbose('Sending events to client')
+      return res.send({ events, count })
+    })
 }
 
 module.exports = router
--- schema/account.js
+++ schema/account.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const balanceSchema = new Schema({
+const accountSchema = new Schema({
   _id: { type: String, required: true },
   balance: { type: String }
 })
 
-module.exports = balanceSchema
+module.exports = accountSchema
