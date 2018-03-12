e85105d639be78feda3ca55e656b408eefff9ca1
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2018-03-12 16:42:19 -0300

    Remove private config settings

--- config.js
+++ config.js
@@ -46,8 +46,7 @@ const config = milieu('mtn', {
     webSocketUrl: 'ws://eth.bloqrock.net:8546',
     tokenAddress: '0x825a2ce3547e77397b7eac4eb464e2edcfaae514',
     auctionAddress: '0x9aeb1035b327f4f81198090f4183f21ca6fcb040',
-    exportStartBlock: 0,
-    tokenDecimals: 18
+    exportStartBlock: 0
   }
 })
 
