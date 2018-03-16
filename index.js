0c277d5a28a904bab4e07477eeb5a1d595cbdd9f
Gabriel Montes <gabriel@bloq.com>
2018-03-16 12:55:22 -0300

    Log exit event

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const beforeExit = require('before-exit')
+
 const config = require('./config')
 const logger = require('./logger')
 const MtnApi = require('./lib')
@@ -10,5 +12,8 @@ const loggeableConfig = Object.assign({}, config)
 delete loggeableConfig.__$
 logger.info('API is starting', loggeableConfig)
 
+beforeExit.do(function (signal) {
+  logger.error('Shutting down API on signal', signal)
+})
 
 module.exports = new MtnApi(config, logger)
